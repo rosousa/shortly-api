6e12bb5db2f319454665f589af32a403770c285d
Rodolfo Sousa <rodolfozsousa@gmail.com>
2022-10-16 20:32:37 -0300

    fix: database connection type

--- src/database/db.js
+++ src/database/db.js
@@ -7,9 +7,6 @@ const { Pool } = pg;
 
 const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 };
 
 const connection = new Pool(databaseConfig);
